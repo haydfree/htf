04eb1c517bc3cecca9a61542353c51e187d7d64d
Hayden Freeman <haydenfreeman@yahoo.com>
2025-06-09 10:41:11 -0500

    AUTO COMMIT: 2025-06-09 10:41:11

--- include/htf_memory.h
+++ include/htf_memory.h
@@ -9,12 +9,12 @@
 static inline Result_void
 htf_zero_memory(void *ptr, u64 size) 
 {
-	Result_void ret = Err_void("htf_zero_memory");
+	Result_void ret = Err_void("unknown error in htf_zero_memory");
 	u8 *p = (u8*)NULL;
 	u64 i = 0;
 
-	GUARD_NULL(ptr);
-	GUARD_NON_POS(size);
+	GUARD_NULL(ptr, void, "ptr passed to htf_zero_memory is NULL");
+	GUARD_NON_POS(size, void, "size passed to htf_zero_memory is non-positive");
 
 	p = (u8*)ptr;
 	for (; i < size; i++) { p[i] = 0; }
