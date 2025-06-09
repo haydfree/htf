99d05a341abe7a22fb3eab31486c3f8a8856088a
Hayden Freeman <haydenfreeman@yahoo.com>
2025-06-09 11:28:02 -0500

    AUTO COMMIT: 2025-06-09 11:28:02

--- test/test.c
+++ test/test.c
@@ -1,14 +1,21 @@
 #include <test.h>
 
-Result_void
-main(void)
+Result_void 
+run_tests(void) 
 {
-	Result_void ret = Err_void("unknown error in main");
+	Result_void ret = Err_void("unknown error in run_tests");
 
-	ZERO_MEMORY(NULL, 0);
-	HTF_LOG_INFO("hello from test.c");
+    ZERO_MEMORY(NULL, 0);
+    HTF_LOG_INFO("hello from test.c");
 
 	ret = Ok_void((VOID){0});
 cleanup:
 	return ret;
+}
+
+int 
+main(void) 
+{
+    Result_void result = run_tests();
+    return is_ok_void(result) ? 0 : 1;
 }
